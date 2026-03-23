1f22f36d009c28b4b12b1754d420bb9655913ab9
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2026-03-24 01:12:40 +0600

    Added range filter based filtering

--- src/Pages/ProductsPage.jsx
+++ src/Pages/ProductsPage.jsx
@@ -16,6 +16,12 @@ const ProductsPage = () => {
   // for details modal
   const [selectedProduct,setSelectedProduct] = useState(null);
 
+  // for range price
+  const [priceRange,setPriceRange] = useState({
+    min: 0,
+    max:1000
+  });
+
 
   useEffect(() => {
     const handleCartUpdate = () => {
@@ -67,6 +73,12 @@ const ProductsPage = () => {
     return item ? item.productQuantity : 0;
   }
 
+  const filteredProducts = productData.filter((product) => {
+    return product.price >= priceRange.min && product.price <= priceRange.max;
+  })
+
+  const percentage = (priceRange.max / 1000) * 100;
+
 
  return (
     <>
@@ -74,57 +86,80 @@ const ProductsPage = () => {
       <div className='my-15'>
         <h2 className='section_title text-center text-2xl lg:text-3xl font-bold mb-2 text-[#0A400C] relative'>Products</h2>
         <p className='text-lg lg:text-xl font-medium text-center lg:w-162.5 mx-auto mb-15 px-2 md:px-2 lg:px-0'>Lorem Ipsum has been the industry's standard dummy text, when an unknown printer took a galley of type and scrambled it</p>
-        <div className='grid gap-3.75 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-10 md:px-10 lg:px-10 xl:px-0'>
-          {
-            productData.map((product) => (
-              <div key={product.id} className='p-3 rounded-sm border border-gray-600 cursor-pointer hover:-translate-y-2 transition-all duration-300 hover:shadow-2xl'>
-                <div className='object-cover transition-transform duration-300 group-hover:scale-102 relative'>
-                  <img className='rounded-sm' src={product.image} alt="product_image" />
-                  <div className='absolute top-2 left-2 hidden group-hover:block'>
-                    <button 
-                      onClick={() => addItems(product)}
-                      className='shadow-[0_15px_15px_rgba(0,0,0,0.10)] bg-[#0a400C]/75 text-white p-1 text-md font-semibold rounded-md cursor-pointer'>
-                      Add To Cart
-                    </button>
+        <div className='flex flex-col md:flex-row lg:flex-row justify-center items-center md:items-start lg:items-start gap-5 lg:gap-10 xl:gap-10'>
+          <div className="w-75 md:w-120 md:mx-5 lg:mx-10">
+            <input
+              type="range"
+              min="0"
+              max="1000"
+              value={priceRange.max}
+              onChange={(e) =>
+                setPriceRange({ ...priceRange, max: Number(e.target.value) })
+              }
+              className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer accent-[#0A400C]"
+              style={{
+              background: `linear-gradient(to right, #0A400C ${percentage}%, #e5e7eb ${percentage}%)`
+            }}
+            />
+            <label className="block mb-2 font-semibold text-gray-700 whitespace-nowrap">
+              Max Price: 
+              <span className="ml-1 text-[#0A400C] font-bold">
+                ${priceRange.max}
+              </span>
+            </label>
+          </div>
+          <div className='grid gap-3.75 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-10 md:px-10 lg:px-10 xl:px-0'>
+            {
+              filteredProducts.map((product) => (
+                <div key={product.id} className='p-3 rounded-sm border border-gray-600 cursor-pointer hover:-translate-y-2 transition-all duration-300 hover:shadow-2xl group'>
+                  <div className='object-cover transition-transform duration-300 group-hover:scale-102 relative'>
+                    <img className='rounded-sm' src={product.image} alt="product_image" />
+                    <div className='absolute top-2 left-2 hidden group-hover:block'>
+                      <button 
+                        onClick={() => addItems(product)}
+                        className='shadow-[0_15px_15px_rgba(0,0,0,0.10)] bg-[#0a400C]/75 text-white p-1 text-md font-semibold rounded-md cursor-pointer'>
+                        Add To Cart
+                      </button>
+                    </div>
+                  </div>
+                  <div>
+                    <div className='flex justify-between items-center mt-2'>
+                      <h3 className='my-2 text-xl font-bold text-[#0A400C]'>{product.name}</h3>
+                      <h2 className='text-xl font-bold text-[#0A400C]'>{product.price}$</h2>
+                    </div>
+
+                    <div className='flex gap-1 items-center text-xl'>
+                      <h4 className='text-md md:text-lg lg:text-lg font-medium text-gray-600'>Average Rating:</h4>
+                      {
+                        [1,2,3,4,5].map((star) => (
+                          <span key={star}>
+                            {
+                              star <= product.rating ? <FaStar className='text-green-900 text-lg md:text-xl lg:text-xl'/> : <FaRegStar className='text-lg md:text-xl lg:text-xl group-hoverr:text-white'/>
+                            }
+                          </span>
+                        ))
+                      }
+                    </div>
+
+                    <h3 className='mt-2 font-bold'> <span className='italic font-medium'>Max Discount: </span>{product.discount}%</h3>
+
+                    <div className="mt-4 border-t-2 border-dashed border-gray-600"></div>
+
+                    <div className='mt-4 mb-2'>
+                      <button onClick={
+                        ()=>{
+                          setSelectedProduct(product);
+                          document.getElementById('detailsModal').showModal()
+                        }}
+                        className='bg-transparent text-gray-800 border-2 border-[#0A400C] rounded-lg p-2 cursor-pointer group-hover:bg-[#0A400C] group-hover:text-white transition-all duration-300'>
+                          View Details
+                      </button>
+                    </div>
                   </div>
                 </div>
-                <div>
-                  <div className='flex justify-between items-center mt-2'>
-                    <h3 className='my-2 text-xl font-bold text-[#0A400C]'>{product.name}</h3>
-                    <h2 className='text-xl font-bold text-[#0A400C]'>{product.price}$</h2>
-                  </div>
-
-                  <div className='flex gap-1 items-center text-xl'>
-                    <h4 className='text-md md:text-lg lg:text-lg font-medium text-gray-600'>Average Rating:</h4>
-                    {
-                      [1,2,3,4,5].map((star) => (
-                        <span key={star}>
-                          {
-                            star <= product.rating ? <FaStar className='text-green-900 text-lg md:text-xl lg:text-xl'/> : <FaRegStar className='text-lg md:text-xl lg:text-xl group-hoverr:text-white'/>
-                          }
-                        </span>
-                      ))
-                    }
-                  </div>
-
-                  <h3 className='mt-2 font-bold'> <span className='italic font-medium'>Max Discount: </span>{product.discount}%</h3>
-
-                  <div className="mt-4 border-t-2 border-dashed border-gray-600"></div>
-
-                  <div className='mt-4 mb-2'>
-                    <button onClick={
-                      ()=>{
-                        setSelectedProduct(product);
-                        document.getElementById('detailsModal').showModal()
-                      }}
-                      className='bg-transparent text-gray-800 border-2 border-[#0A400C] rounded-lg p-2 cursor-pointer group-hover:bg-[#0A400C] group-hover:text-white transition-all duration-300'>
-                        View Details
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))
-          }
+              ))
+            }
+          </div>
         </div>
 
         <dialog id="detailsModal" className="modal">
